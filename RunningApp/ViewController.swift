955ad0d67fa14e928855cb88cb3b1727b77fbedc
Alberto Aldana <albertoaldanar@hotmail.com>
2021-04-13 11:35:36 -0600

    initialized running app

---
+++ RunningApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RunningApp
+//
+//  Created by Alberto Aldana on 13/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
