adee24a98bd470d3d5d097c3bc2a753dbeff0f06
Alberto Aldana <albertoaldanar@hotmail.com>
2021-04-14 15:37:02 -0600

    Created swift class for each tab screen

---
+++ RunningApp/LogrosViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RetosViewController.swift
+//  RunningApp
+//
+//  Created by Alberto Aldana on 14/04/21.
+//
+
+import UIKit
+
+class LogrosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Logros view controller loaded")
+    }
+
+
+}
---
+++ RunningApp/NotifViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RetosViewController.swift
+//  RunningApp
+//
+//  Created by Alberto Aldana on 14/04/21.
+//
+
+import UIKit
+
+class NotifViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Notif view controller loaded")
+    }
+
+
+}
