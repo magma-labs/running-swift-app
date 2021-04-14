e10eddcb3f40c90ecebb983b4444f6a1a607318e
Fernando <fernandoamz148@gmail.com>
2021-04-14 14:51:24 -0500

    Add google maps

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'RunningApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for RunningApp
+  pod 'GoogleMaps'
+  
+  target 'RunningAppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'RunningAppUITests' do
+    # Pods for testing
+  end
+
+end
--- RunningApp/ViewController.swift
+++ RunningApp/ViewController.swift
@@ -6,14 +6,26 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var google_map: GMSMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let camera = GMSCameraPosition(latitude: 19.0522, longitude: -104.316, zoom: 6.0)
+        google_map.camera = camera
+        self.show_marker(position: google_map.camera.target)
     }
-
-
+    
+    func show_marker(position: CLLocationCoordinate2D) {
+        let marker = GMSMarker()
+        marker.position = position
+        marker.title = "Colima"
+        marker.snippet = "Capital Of Manzanillo"
+        marker.map = google_map
+    }
 }
 
