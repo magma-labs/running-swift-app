127946f289e7ba48340a3d4cd8aa26c2cc53bde5
Fernando <fernandoamz148@gmail.com>
2021-04-20 17:38:31 -0500

    Add format for hour minutes and seconds

--- RunningApp/ViewController.swift
+++ RunningApp/ViewController.swift
@@ -10,7 +10,12 @@ import GoogleMaps
 
 class ViewController: UIViewController {
     @IBOutlet weak var google_map: GMSMapView!
+    @IBOutlet weak var startTrackingBtn: UIButton!
+    @IBOutlet weak var LabelTrack: UILabel!
     
+    var OurTimer = Timer()
+    var TimerDisplayed:Int = 0;
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,6 +23,8 @@ class ViewController: UIViewController {
         let camera = GMSCameraPosition(latitude: 19.0522, longitude: -104.316, zoom: 6.0)
         google_map.camera = camera
         self.show_marker(position: google_map.camera.target)
+        
+        startTrackingBtn.layer.cornerRadius = 15;
     }
     
     func show_marker(position: CLLocationCoordinate2D) {
@@ -26,6 +33,31 @@ class ViewController: UIViewController {
         marker.title = "Colima"
         marker.snippet = "Capital Of Manzanillo"
         marker.map = google_map
+    }
+    
+    @IBAction func startTrackingBtn(_ sender: Any) {
+        OurTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(Action), userInfo: nil, repeats: true)
+    }
+    
+    @objc func Action() {
+        TimerDisplayed += 1
+        let time = secondsToHoursMinutesSeconds(seconds: TimerDisplayed)
+        let timeString = makeTimeString(hours: time.0, minutes: time.1, seconds: time.2)
+        LabelTrack.text = String(timeString)
+    }
+    
+    func secondsToHoursMinutesSeconds(seconds: Int) -> (Int, Int, Int) {
+        return ((seconds / 3600), ((seconds % 3600) / 60),((seconds % 3600) % 60))
+    }
+        
+    func makeTimeString(hours: Int, minutes: Int, seconds : Int) -> String {
+        var timeString = ""
+        timeString += String(format: "%02d", hours)
+        timeString += " : "
+        timeString += String(format: "%02d", minutes)
+        timeString += " : "
+        timeString += String(format: "%02d", seconds)
+        return timeString
     }
 }
 
