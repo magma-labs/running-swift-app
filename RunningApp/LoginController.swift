913de2ee490b1e8b2a67675f3bec932e6540301c
Fernando <fernandoamz148@gmail.com>
2021-04-29 12:02:41 -0500

    Adding validation for text inputs

--- RunningApp/LoginController.swift
+++ RunningApp/LoginController.swift
@@ -10,19 +10,21 @@ import FirebaseAuth
 
 class LoginController: UIViewController {
     @IBOutlet weak var lblErrors: UILabel!
+    @IBOutlet weak var emailInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func SignIn(_ sender: Any) {
-        let email = "fernando@magma.io"
-        let password = "testing123"
+        let email = emailInput.text!
+        let password = passwordInput.text!
         
         Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
-          guard let strongSelf = self else { return }
+            guard self != nil else { return }
             
-            if let error = error as? NSError {
+            if let error = error as NSError? {
               switch AuthErrorCode(rawValue: error.code) {
                   case .invalidEmail:
                     self?.lblErrors?.text = "Invalid Email"
@@ -30,9 +32,10 @@ class LoginController: UIViewController {
                     self?.lblErrors?.text = "Contraseña incorrecta"
               }
             } else {
+              // This variables are getting by auth method
               let newUserInfo = Auth.auth().currentUser
               let email = newUserInfo?.email
-            
+                
               let next = self?.storyboard?.instantiateViewController(withIdentifier: "MainController") as! UITabBarController
               self?.present(next, animated: true)
             }
